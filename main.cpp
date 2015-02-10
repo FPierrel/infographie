c7c10dd395cc0f18b79fdd4d4f8f70ee91d7561c
pierrel14u <pierrel14u@fst-ats-315-l09.fst.site.univ-lorraine.fr>
2015-02-10 14:43:06 +0100

    Correction Projection

--- main.cpp
+++ main.cpp
@@ -44,7 +44,10 @@ Matrix lookat(Vec3f eye, Vec3f center, Vec3f up){
     return res;
 }
 
-void triangle_plein(TGAImage &image, int **zbuffer,Pos_b pos_1, Pos_b pos_2, Pos_b pos_3,
+void triangle_plein(TGAImage &image, Model *model,int **zbuffer,Pos_b pos_1, Pos_b pos_2, Pos_b pos_3,
+                                    vector<float> uv1,
+                                    vector<float> uv2,
+                                    vector<float> uv3,
                                     TGAColor color1 = TGAColor(255, 1),
                                     TGAColor color2 = TGAColor(255, 1 ),
                                     TGAColor color3 = TGAColor(255, 1))
@@ -80,6 +83,7 @@ void triangle_plein(TGAImage &image, int **zbuffer,Pos_b pos_1, Pos_b pos_2, Pos
                 {
                     //Calcul coordonée barycentric
                     // (http://imagine.inrialpes.fr/people/Damien.Rohmer/documents/teaching/11_1spring_cpe/4eti_mso_synthese/tp/tp_2_projectif.pdf)
+
                     u = ((x-pos_3.x)*(pos_2.y-pos_3.y)-(pos_2.x-pos_3.x)*(y-pos_3.y))/
                         ((pos_1.x-pos_3.x)*(pos_2.y-pos_3.y)-(pos_2.x-pos_3.x)*(pos_1.y-pos_3.y));
 
@@ -89,8 +93,13 @@ void triangle_plein(TGAImage &image, int **zbuffer,Pos_b pos_1, Pos_b pos_2, Pos
                     w = ((x-pos_2.x)*(pos_1.y-pos_2.y)-(pos_1.x-pos_2.x)*(y-pos_2.y))/
                         ((pos_1.x-pos_3.x)*(pos_2.y-pos_3.y)-(pos_2.x-pos_3.x)*(pos_1.y-pos_3.y));
 
-                    c = u*color1.val + v*color2.val + w*color3.val;
-                    image.set(x,y,TGAColor(c,1));
+                    float xt = u*uv1[0] + v*uv2[0] + w*uv3[0];
+                    float yt = u*uv1[1] + v*uv2[1] + w*uv3[1];
+
+                    //c = u*color1.val + v*color2.val + w*color3.val;
+
+                    image.set(x,y,model->diffusemap_.get(xt*model->diffusemap_.get_width(),
+                                                         yt*model->diffusemap_.get_height()));
                     zbuffer[x][y] = z;
                 }
             }
@@ -99,32 +108,31 @@ void triangle_plein(TGAImage &image, int **zbuffer,Pos_b pos_1, Pos_b pos_2, Pos
 
 void rendu(TGAImage &image, Model model)
 {
-
     int w = image.get_width();
     int h = image.get_height();
     float lum1, lum2, lum3;
 
-
     vector<vector<int> > face;
     Vec3f_b lumiere(0.f,0.f,-1.f);
     Vec3f_b vn1, vn2, vn3;
+    vector<float> uv1, uv2, uv3;
     Pos_b pos_1, pos_2, pos_3;
 
-    Vec3f eye(0,0,3);
+    Vec3f eye(1,1,1);
     Vec3f center(0,0,0);
     Vec3f up(0,1,0);
 
     Matrix ModelView(lookat(eye,center,Vec3f(0,1,0)));
     Matrix Projection = Matrix::identity(4);
     Matrix ViewPort   = viewport(w/8, h/8, w*3/4, h*3/4);
-    Projection[3][2] = -1.f/(eye-center).norm();
-    Matrix z = (ViewPort*Projection*ModelView);
+    Projection[2][3] = -1.f/(eye-center).norm();
+    //Matrix z = (ViewPort*Projection*ModelView);
     /*lookat(eye,center,up);
     viewport(w/8, h/8, w*3/4, h*3/4);
     projection(-1.f/(eye-center).norm());*/
 
-    //float zw = (0.5 * w);
-    //float zh = (0.5 * h);
+    float zw = (0.5 * w);
+    float zh = (0.5 * h);
 
     int** zbuffer;
     zbuffer = new int*[w];
@@ -138,15 +146,15 @@ void rendu(TGAImage &image, Model model)
     for(unsigned int i = 0 ; i < model.faces.size() ; i++)
     {
         face = model.faces[i];
-        /*pos_1.x = (model.sommets[face[0][0]-1].x*zw) + w/2;
-        pos_2.x = (model.sommets[face[1][0]-1].x*zw) + w/2;
-        pos_3.x = (model.sommets[face[2][0]-1].x*zw) + w/2;
-        pos_1.y = (model.sommets[face[0][0]-1].y*zh) + h/2;
-        pos_2.y = (model.sommets[face[1][0]-1].y*zh) + h/2;
-        pos_3.y = (model.sommets[face[2][0]-1].y*zh) + h/2;
-        pos_1.z = (model.sommets[face[0][0]-1].z*2048);
-        pos_2.z = (model.sommets[face[1][0]-1].z*2048);
-        pos_3.z = (model.sommets[face[2][0]-1].z*2048);*/
+//        pos_1.x = (model.sommets[face[0][0]-1].x*zw) + w/2;
+//        pos_2.x = (model.sommets[face[1][0]-1].x*zw) + w/2;
+//        pos_3.x = (model.sommets[face[2][0]-1].x*zw) + w/2;
+//        pos_1.y = (model.sommets[face[0][0]-1].y*zh) + h/2;
+//        pos_2.y = (model.sommets[face[1][0]-1].y*zh) + h/2;
+//        pos_3.y = (model.sommets[face[2][0]-1].y*zh) + h/2;
+//        pos_1.z = (model.sommets[face[0][0]-1].z*2048);
+//        pos_2.z = (model.sommets[face[1][0]-1].z*2048);
+//        pos_3.z = (model.sommets[face[2][0]-1].z*2048);
 
         Matrix facette(4,4);
         facette[0][0] = model.sommets[face[0][0]-1].x + 1;// + w/2;
@@ -172,9 +180,14 @@ void rendu(TGAImage &image, Model model)
         pos_2.z = facette[2][1];
         pos_3.z = facette[2][2];
 
+        uv1 = model.uv[face[0][1]-1];
+        uv2 = model.uv[face[1][1]-1];
+        uv3 = model.uv[face[2][1]-1];
+
         vn1 = model.norms[face[0][2]-1];
         vn2 = model.norms[face[1][2]-1];
         vn3 = model.norms[face[2][2]-1];
+
         lumiere.normalize();
         vn1.normalize();
         vn2.normalize();
@@ -192,7 +205,8 @@ void rendu(TGAImage &image, Model model)
         if (lum3 > 1.0f) lum3= 1.0f;
         else if (lum3 < 0.f) lum3 = 0.0f;
 
-        triangle_plein(image, zbuffer, pos_1,pos_2,pos_3, TGAColor(lum1*255,1),
+        triangle_plein(image, &model, zbuffer, pos_1,pos_2,pos_3, uv1, uv2, uv3,
+                                                          TGAColor(lum1*255,1),
                                                           TGAColor(lum2*255,1),
                                                           TGAColor(lum3*255,1));
     }
@@ -204,8 +218,8 @@ void rendu(TGAImage &image, Model model)
 
 int main()
 {
-    Model model("head.obj");
-    TGAImage image(1000, 1000, 1);
+    Model model("african_head.obj");
+    TGAImage image(1024, 1024, TGAImage::RGB);
 
     //wireframe(image, model);
     rendu(image,model);
